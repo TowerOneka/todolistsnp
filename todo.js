4f3b09232b32b00d8bcc6e16cdfeec5198a46172
TowerOneka <63141840+TowerOneka@users.noreply.github.com>
2021-09-20 06:12:53 +0300

    Add files via upload

--- todo.js
+++ todo.js
@@ -10,8 +10,8 @@ let todoViewer = () =>{
     let todo = '';
     todoList.forEach((i, index)=>{
         todo += `<li id='item_${index}'>
-            <input type='checkbox' id='id_${index}' ${i.checked ? 'checked' : ''}>
-            <label for='id_${index}'>${i.todo}</label>
+            <input type='checkbox' onchange="checkbox(${index})" id='id_${index}' ${i.checked ? 'checked' : ''}>
+            <label for='id_${index}' onselectstart="return false" ondblclick="changelement(${index})">${i.todo}</label>
             <button id='todo__remove' onclick="removeTodo(item_${index},${i.todo})">X</button>
         </li>`;
         list.innerHTML = todo;
@@ -51,9 +51,8 @@ form.addEventListener('submit', (e)=>{
     input.value = '';
 });
 
-list.addEventListener('change', (e) => {
-    let idTodo = e.target.getAttribute('id');
-    let label = list.querySelector('[for='+ idTodo +']');
+let checkbox = (index) => {
+    let label = list.querySelector('[for=id_'+ index +']');
     let labelValue = label.innerHTML;
 
     todoList.forEach((item)=>{
@@ -62,7 +61,7 @@ list.addEventListener('change', (e) => {
             localStorage.setItem('todo', JSON.stringify(todoList));
         }
     })
-})
+}
 
 let removeTodo = (element,label) =>{
     todoList.forEach((item, index)=>{
@@ -72,4 +71,43 @@ let removeTodo = (element,label) =>{
         }
     })
     localStorage.setItem('todo', JSON.stringify(todoList));
+}
+
+let changelement = (index) =>{
+    let editinput = document.createElement('input');
+    let label = document.querySelector('[for=id_'+ index +']');
+    editinput.className = 'edit';
+    let text = label.innerHTML;
+    editinput.value = text;
+    let element = document.querySelector('#item_' + index);
+    
+    element.replaceWith(editinput);
+    editinput.focus();
+    editinput.addEventListener('focusout', ()=>{
+        if(!editinput.value){
+             editinput.replaceWith(element);
+             return;
+        };
+        let error = false
+        todoList.forEach((item)=>{
+            if(editinput.value == item.todo) {
+                error = true;
+                
+            }
+        });
+        if (error) {
+            editinput.replaceWith(element);
+            return;
+        }
+
+        todoList.forEach((item)=>{
+            if (item.todo == text){
+                item.todo = editinput.value;
+                label.innerHTML = editinput.value;
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        })
+        editinput.replaceWith(element);
+    });
+    
 }
